5fbaa5dd1df177c6ed9797e3c138aa9d09452831
griminir <viktor.elliot.king@gmail.com>
2024-08-04 22:53:57 +0200

    Set up wpf project

---
+++ HotelApp.Desktop/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Databases;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<ISqlDataAccess, SqlDataAccess>();
+            services.AddTransient<IDatabaseData, SqlData>();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = builder.Build();
+
+            services.AddSingleton(config);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+
+            mainWindow.Show();
+        }
+    }
+
+}
