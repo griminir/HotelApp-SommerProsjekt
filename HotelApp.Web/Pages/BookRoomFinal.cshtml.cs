d15c1c0e056419f663306578969149d36f3fe2dd
griminir <viktor.elliot.king@gmail.com>
2024-08-04 14:51:56 +0200

    sitter fast på hvordan convertere date time til noe som databasen vill godkjenne

--- HotelApp.Web/Pages/BookRoomFinal.cshtml.cs
+++ HotelApp.Web/Pages/BookRoomFinal.cshtml.cs
@@ -1,10 +1,15 @@
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelApp.Web.Pages
 {
     public class BookRoomFinalModel : PageModel
     {
+        private readonly IDatabaseData _db;
+
         [BindProperty(SupportsGet = true)]
         public int RoomTypeId { get; set; }
 
@@ -20,13 +25,24 @@ namespace HotelApp.Web.Pages
         [BindProperty]
         public string LastName { get; set; }
 
+        public RoomTypeModel RoomType { get; set; }
+
+        public BookRoomFinalModel(IDatabaseData db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
-
+            if (RoomTypeId > 0)
+            {
+                RoomType = _db.GetRoomTypeById(RoomTypeId);
+            }
         }
 
         public IActionResult OnPost()
         {
+            _db.BookGuest(FirstName, LastName, StartDate, EndDate, RoomTypeId);
             return RedirectToPage("/index");
         }
     }
